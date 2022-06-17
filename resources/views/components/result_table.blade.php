077c39fc4b8a4dc81daecfd0be75949be90543ca
Allwell Onen <aleenfestus@gmail.com>
2022-06-17 22:37:18 +0100

    Update results table ui

--- resources/views/components/result_table.blade.php
+++ resources/views/components/result_table.blade.php
@@ -1,16 +1,16 @@
 @props(['election' => $election])
 
 <tr
-    class="text-sm text-neutral-600 hover:text-neutral-900 transition duration-300 bg-white cursor-default hover:bg-neutral-100/50">
-    <td class="py-3 text-left cursor-default w-fit" title="{{ $election->title }}">
+    class="text-sm text-neutral-600 cursor-default hover:text-neutral-900 transition duration-300 bg-white hover:bg-neutral-100/50">
+    <td class="py-4 text-left w-fit" title="{{ $election->title }}">
         <div class="px-2 line-clamp-1">{{ $election->title }}</div>
     </td>
 
-    <td class="py-3 border-x text-left cursor-default border-neutral-200" title="{{ $election->description }}">
-        <div class="pl-2 line-clamp-1">{{ $election->description }}</div>
+    <td class="py-4 border-x text-left border-neutral-100" title="{{ $election->description }}">
+        <div class="px-4 line-clamp-1">{{ $election->description }}</div>
     </td>
 
-    <td class="py-3 text-center capitalize cursor-default border-neutral-200">
+    <td class="py-4 text-center capitalize">
         <a href="{{ route('results.view', $election->id) }}" type="submit" title="View results for this election"
             class="font-semibold text-indigo-600 capitalize bg-transparent border-0 outline-none dark:text-indigo-500 hover:underline">
             View
--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -5,52 +5,18 @@
 
 @section('views')
     <div class="w-full px-1">
-        {{-- <x-live_heading text="Results" /> --}}
-
-
-        {{-- <div class="flex-grow pb-10 overflow-x-auto mt-7 text-neutral-500 dark:text-neutral-200">
-            @if ($closed_elections->count() > 0)
-                <table class="w-full mb-8 text-sm bg-white text-left text-neutral-500 dark:text-neutral-400">
-                    <thead class="text-xs border-b border-neutral-200 uppercase text-neutral-700">
-                        <tr>
-                            <th scope="col" class="p-4 text-left">
-                                Election
-                            </th>
-
-                            <th scope="col" class="py-4 pl-2 text-left">
-                                Description
-                            </th>
-
-                            <th scope="col" class="p-4 text-center">
-                                Action
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($closed_elections as $closed)
-                            <x-result_table :election="$closed" />
-                        @endforeach
-                    </tbody>
-                </table>
-            @else
-                <div class="w-full h-full mt-10 text-base text-center sm:text-lg text-neutral-500 dark:text-neutral-400">
-                    No results yet.
-                </div>
-            @endif
-        </div> --}}
-
-
         <div class="w-full px-3 pb-3 bg-white overflow-y-auto overflow-x-auto">
             <table class="w-full">
                 <thead>
-                    <tr class="border-b border-neutral-200 text-sm">
-                        <th class="text-neutral-700 text-sm uppercase px-2 text-left">Election Title</th>
-                        <th class="text-neutral-700 text-sm uppercase px-2 py-4 text-left border-x">Description</th>
+                    <tr class="border-b border-neutral-100 text-sm">
+                        <th class="text-neutral-700 text-sm uppercase pr-2 text-left">Election Title</th>
+                        <th class="text-neutral-700 text-sm uppercase px-4 py-4 text-left border-x border-neutral-100">
+                            Description</th>
                         <th class="text-neutral-700 text-sm uppercase px-2 py-4 text-center">Actions</th>
                     </tr>
                 </thead>
                 @if ($closed_elections->count() > 0)
-                    <tbody>
+                    <tbody class="border-b border-neutral-100">
                         @foreach ($closed_elections as $closed)
                             <x-result_table :election="$closed" />
                         @endforeach
@@ -62,9 +28,9 @@
                 @endif
             </table>
 
-            {{-- <div class="px-3 mt-2 border-t border-neutral-200 pt-2.5">
+            <div class="px-3 mt-2.5 ">
                 {{ $closed_elections->links() }}
-            </div> --}}
+            </div>
         </div>
     </div>
 @endsection
