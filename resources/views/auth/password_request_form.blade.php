198acf40b0eb82ff8505413357b54b7058f05670
Allwell Onen <aleenfestus@gmail.com>
2022-07-18 18:06:18 +0100

    Password reset functionality

--- resources/views/auth/password_request_form.blade.php
+++ resources/views/auth/password_request_form.blade.php
@@ -7,16 +7,20 @@
 @section('content')
     <div id="page-content" class="flex items-center justify-center px-6 py-12 login-container lg:px-8">
         <div class="flex flex-col w-full max-w-lg gap-2">
+
+            <x-info_message />
+            <x-error_message />
+
             <div>
                 <h2 class="text-lg font-extrabold text-center uppercase sm:text-xl text-neutral-800">Forgot Password?</h2>
             </div>
 
-            <form id="login-for" class="flex flex-col gap-2 mt-5" action="{{ route('password.reset') }}" method="POST">
+            <form id="login-for" class="flex flex-col gap-2 mt-2" action="{{ route('password.reset.link') }}" method="POST">
                 @csrf
 
-                <x-error_message />
-
-                <span class="text-sm text-neutral-600">Reset password in two quick steps</span>
+                <span class="text-sm text-neutral-600 tracking-wide">
+                    Enter your email and we will send you a link to reset your password.
+                </span>
 
                 <div class="flex flex-col">
                     {{-- <label for="email" class="text-sm font-semibold text-neutral-700">Email</label> --}}
@@ -25,23 +29,28 @@
                         placeholder="Email address" required>
                 </div>
 
-                <div class="flex flex-col w-full gap-2">
+                <div class="flex flex-col w-full gap-2 mt-2">
                     <button type="submit"
                         class="w-full py-3 text-sm text-white bg-indigo-600 rounded hover:bg-indigo-700 focus:bg-indigo-700 focus:ring focus:ring-indigo-600/40">
-                        Reset password
+                        Request link
                     </button>
 
                     {{-- Link to sign up page --}}
                     <div class="flex">
-                        <a class="flex items-center justify-start py-1 px-1.5 hover:bg-neutral-100 text-base font-medium text-indigo-600 rounded focus:bg-neutral-100 focus:ring focus:ring-neutral-200"
+                        <a class="flex items-center justify-start text-sm font-medium text-indigo-600 hover:underline"
                             href="{{ route('login') }}">
-                            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                            <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                                 xmlns="http://www.w3.org/2000/svg">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7">
                                 </path>
                             </svg>
                             Back to home</a>
                     </div>
+                </div>
+
+
+                <div class="text-sm text-neutral-500 tracking-wide">
+                    If you don't see the email in your inbox, check your spam folder.
                 </div>
             </form>
         </div>
