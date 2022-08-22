5474b0f87e8abbe5977c010e3ad9cc1bff024073
Allwell Onen <aleenfestus@gmail.com>
2022-08-22 04:08:34 +0100

    Update results page

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -5,11 +5,16 @@
 @section('home-page')
     <div class="flex flex-col w-full gap-5 p-4 bg-white rounded-xl sm:p-5">
         <h2 class="text-3xl text-[#0000FF] font-bold">Election Results</h2>
-
-        <div class="flex flex-col w-full gap-5">
-            @foreach ($elections as $election)
-                <x-result_card :election="$election" />
-            @endforeach
-        </div>
+        @if ($elections->count() > 0)
+            <div class="flex flex-col w-full gap-5">
+                @foreach ($elections as $election)
+                    <x-result_card :election="$election" />
+                @endforeach
+            </div>
+        @else
+            <div class="w-full py-5 flex justify-center items-center">
+                <span class="text-base text-neutral-500 tracking-wider">No results.</span>
+            </div>
+        @endif
     </div>
 @endsection
